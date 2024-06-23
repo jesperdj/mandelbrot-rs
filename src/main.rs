6bed6316f1181a8bdbb319a12d127b8f6b926f74
Jesper de Jong <jespdj@gmail.com>
2024-06-23 21:52:32 +0200

    render_image function.

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,18 @@
 
 #![allow(dead_code)]
 
+use std::ops::{AddAssign, Div, Mul};
 use std::time::Instant;
 
 use image::{Rgb, RgbImage};
 use num_complex::Complex64;
 
 use crate::palette::{Entry, Palette, TablePalette};
-use crate::reconstruction::filter::MitchellFilter;
+use crate::reconstruction::filter::{Filter, MitchellFilter};
 use crate::reconstruction::Reconstructor;
 use crate::rendering::mandelbrot::MandelbrotRenderer;
 use crate::rendering::Renderer;
+use crate::sampling::Sampler;
 use crate::sampling::stratified::StratifiedSampler;
 
 mod palette;
@@ -62,20 +64,35 @@ fn main() {
     let filter = MitchellFilter::with_defaults();
     // let filter = BoxFilter::with_defaults();
 
+    let result_to_color = |value| palette.evaluate(value);
+
+    let image = render_image(&sampler_factory, &renderer, &filter, &result_to_color, width, height);
+    image.save("mandelbrot.png").unwrap();
+}
+
+fn render_image<SF, S, R, RR, F, M>(sampler_factory: &SF, renderer: &R, filter: &F, result_to_color: &M, width: u32, height: u32) -> RgbImage
+where
+    SF: Fn(u32, u32) -> S + Sync,
+    S: Sampler,
+    R: Renderer<RR> + Sync,
+    RR: Copy + Default + AddAssign + Mul<f64, Output=RR> + Div<f64, Output=RR>,
+    F: Filter + Sync,
+    M: Fn(RR) -> Rgb<u8> + Sync,
+{
     let start_time = Instant::now();
-    let image_buffer = RgbImage::from_par_fn(width, height, |x, y| {
+    let image = RgbImage::from_par_fn(width, height, |x, y| {
         let sampler = sampler_factory(x, y);
-        let mut reconstructor = Reconstructor::new(&filter);
+        let mut reconstructor = Reconstructor::new(filter);
 
         for sample in sampler {
             let result = renderer.render(&sample);
             reconstructor.accumulate(&sample, result);
         }
 
-        palette.evaluate(reconstructor.value())
+        result_to_color(reconstructor.value())
     });
     let duration = Instant::now().duration_since(start_time).as_millis();
     println!("Rendering time: {} ms", duration);
 
-    image_buffer.save("mandelbrot.png").unwrap();
+    image
 }
